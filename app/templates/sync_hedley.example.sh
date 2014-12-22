3e59d89a340c936053164a4b13ee7900fd6d7aff
Amitai Burstein <amitai@gizra.com>
2014-12-22 20:52:04 +0200

    More work

--- app/templates/sync_hedley.example.sh
+++ app/templates/sync_hedley.example.sh
@@ -9,14 +9,15 @@
 
 
 GENERATOR_FOLDER="/var/www/generator-hedley"
-PROFILE_NAME="skeleton"
 
 # Client
-rsync -avz --exclude '.*' ./client $GENERATOR_FOLDER/client
+rsync -avz --exclude '.*' --exclude 'client/node_modules' --exclude 'client/bower_components' ./client $GENERATOR_FOLDER/client
 
 # Behat
-rsync -avz --exclude 'bin', 'vendor', 'behat.local.yml' ./behat $GENERATOR_FOLDER/behat
+rsync -avz --exclude 'behat/bin', 'behat/vendor', 'behat/behat.local.yml' ./behat $GENERATOR_FOLDER/behat
 
 # Drupal - we make sure to hardcode the copy to "skeleton".
-rsync -avz --exclude 'contrib/' ./skeleton $GENERATOR_FOLDER/skeleton
+rsync -avz --exclude 'skeleton/modules/contrib' --exclude 'skeleton/themes/contrib' --exclude 'skeleton/libraries' ./skeleton $GENERATOR_FOLDER/skeleton
+
+
 
