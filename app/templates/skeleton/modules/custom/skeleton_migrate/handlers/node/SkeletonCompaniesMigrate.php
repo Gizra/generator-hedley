3ce9eb76db6258bdf6294c1d96bb9baa8831505a
Amitaibu <amitai@gizra.com>
2014-12-25 12:03:18 +0200

    Add company images

--- app/templates/skeleton/modules/custom/skeleton_migrate/handlers/node/SkeletonCompaniesMigrate.php
+++ app/templates/skeleton/modules/custom/skeleton_migrate/handlers/node/SkeletonCompaniesMigrate.php
@@ -16,9 +16,27 @@ class SkeletonCompaniesMigrate extends \SkeletonMigrateBase {
       ->addFieldMapping(OG_GROUP_FIELD)
       ->defaultValue(TRUE);
 
+    // Group is private by default.
     $this
       ->addFieldMapping(OG_ACCESS_FIELD)
       ->defaultValue(TRUE);
+
+    // Add an image by the unique ID name.
+    $this
+      ->addFieldMapping('field_company_logo')
+      ->defaultValue('company1');
+
+    $this
+      ->addFieldMapping('field_company_logo:file_replace')
+      ->defaultValue(FILE_EXISTS_REPLACE);
+
+    $path = variable_get('skeleton_migrate_directory', FALSE) ? variable_get('skeleton_migrate_directory') : drupal_get_path('module', 'skeleton_migrate');
+    $this
+      ->addFieldMapping('field_company_logo:source_dir')
+      ->defaultValue($path . '/images');
+
+    $this
+      ->addFieldMapping('field_company_logo:destination_dir', 'destination');
 
     // Group belong to the admin by default.
     $this
