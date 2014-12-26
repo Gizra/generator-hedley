45cf26ef0b5c57e13764cb4a62ae5ef8e492ff6c
Amitai Burstein <amitai@gizra.com>
2014-12-26 21:43:31 +0200

    Specific config files.

--- travis/aliases.drushrc.php
+++ travis/aliases.drushrc.php
@@ -2,6 +2,6 @@
 
 $aliases['site'] = array(
   'uri' => 'http://127.0.0.1:8080',
-  'root' => '/home/travis/build/repoName/www',
+  'root' => '/home/travis/build/build/skeleton/www',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );
