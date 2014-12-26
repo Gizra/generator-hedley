9185cdb925346c1546aa6f81dcd8934d4237d4fb
Amitai Burstein <amitai@gizra.com>
2014-12-26 23:57:44 +0200

    d8

--- travis/aliases.drushrc.php
+++ travis/aliases.drushrc.php
@@ -2,6 +2,6 @@
 
 $aliases['site'] = array(
   'uri' => 'http://127.0.0.1:8080',
-  'root' => '/home/travis/build/build/skeleton/www',
+  'root' => '/home/travis/build/repoName/www',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );
