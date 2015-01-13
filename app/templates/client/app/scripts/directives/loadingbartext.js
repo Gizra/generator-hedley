6e11cea01ca5148dbd83a5df6dfab6cc8e1240a9
Carlos Mantilla <cmantilla@aliongroo.com>
2015-01-13 11:24:22 +0200

    feat: add directive loading bar.

--- app/templates/client/app/scripts/directives/loadingbartext.js
+++ app/templates/client/app/scripts/directives/loadingbartext.js
@@ -9,9 +9,32 @@
 angular.module('clientApp')
   .directive('loadingBarText', function () {
     return {
-      link: function postLink(scope, element) {
-        var text = 'loading...';
-        element.prepend('<div class=\"splash-screen\">'  + text +  '</div>');
-      }
+      retrict: 'EA',
+      template: '<div class=\"splash-screen\" ng-show="isLoading">{{text}}</div>',
+      controller: function($scope) {
+        $scope.text = 'loading...';
+        $scope.isLoading = false;
+
+        /**
+         * Determine if the text is showing.
+         *
+         * @param isLoading - Boolean
+         *  True show the text, false hide it.
+         */
+        function setLoading(isLoading) {
+          $scope.isLoading = isLoading;
+        }
+
+        // Events to set the message when start the XHR request until is completed.
+        $scope.$on('cfpLoadingBar:started', function() {
+          setLoading(true);
+        });
+
+        $scope.$on('cfpLoadingBar:completed', function() {
+          setLoading(false);
+        });
+      },
+      // Isolate scope.
+      scope: {}
     };
   });
