2f433965cafb8b9e75df56ad5ef333c1c70d379c
Carlos Mantilla <cmantilla@aliongroo.com>
2015-07-06 10:55:32 +0300

    feat(auth): logout route implementation.

--- app/templates/client/app/scripts/controllers/dashboard.js
+++ app/templates/client/app/scripts/controllers/dashboard.js
@@ -8,21 +8,9 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('DashboardCtrl', function ($scope, account, selectedCompany, Auth, $state, $log) {
+  .controller('DashboardCtrl', function ($scope, account, selectedCompany) {
 
     // Initialize values.
     $scope.companies = account.companies;
     $scope.selectedCompany = selectedCompany ? selectedCompany : parseInt(account.companies[0].id);
-
-
-
-    /**
-     * Logout current user.
-     *
-     * Do whatever cleaning up is required and change state to 'login'.
-     */
-    $scope.logout = function() {
-      Auth.logout();
-      $state.go('login');
-    };
   });
