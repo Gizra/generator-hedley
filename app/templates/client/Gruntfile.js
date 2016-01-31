3b3f0c9066ced76231e7d3c646912022f08dcb9f
Nir Galon <nirgn975@gmail.com>
2016-01-31 11:58:51 +0200

    Change Grunt port.

--- app/templates/client/Gruntfile.js
+++ app/templates/client/Gruntfile.js
@@ -88,7 +88,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-          port: 9001,
+          port: 9000,
           middleware: function (connect) {
             return [
               connect.static('.tmp'),
