8da2a11791f5ec20e9b5f7db27937e6a00f53b55
nader77 <nader@gizra.com>
2016-02-26 14:33:45 +0100

    Automatically revert features when deploying code to Pantheon.

---
+++ app/templates/scripts/revert_all_features.php
@@ -0,0 +1,9 @@
+<?php
+//Revert all features
+echo "Reverting all features...\n";
+passthru('drush fra -y');
+echo "Reverting complete.\n";
+//Clear all cache
+echo "Clearing cache.\n";
+passthru('drush cc all');
+echo "Clearing cache complete.\n";
